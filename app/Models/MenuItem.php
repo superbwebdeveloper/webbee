b80bee0d62ff8fe27e775e50ccb801997c0bc14f
Balbir Kaur <superbwebdeveloper@gmail.com>
2021-05-22 03:43:46 +0530

    Third Question Model Updated

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class MenuItem extends Model
 {
-
+    protected $table = "menu_items";
+    protected $fillable = ['name', 'url', 'parent_id', 'created_at', 'updated_at'];
 }
